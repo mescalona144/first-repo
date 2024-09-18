849b6a0ac14cf8a500182e2af391cecebec8fc06
mescalona <mescalona144@gmail.com>
2024-09-17 21:58:24 -0300

    Podcaster

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content h1, .about-img, .about-dog', {origin: 'left
 ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Educador', 'Podcaster'],
+    strings: ['Frontend Developer', 'Educador', 'Podcaster.'],
     typeSpeed : 100,
     backSpeed : 100,
     backDelay : 1000,
